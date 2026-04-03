3ed4c4f66cb1da71405203e146fc97a208b4478a
Arik Chakma <arikchangma@gmail.com>
2026-04-03 14:20:24 +0600

    chore: consolidate migrations into single initial file

--- examples/api/app/db/migrations/0000_perfect_maximus.sql
+++ examples/api/app/db/migrations/0000_perfect_maximus.sql
@@ -1,3 +1,15 @@
+CREATE TABLE `emails` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`from` text NOT NULL,
+	`to` text NOT NULL,
+	`subject` text DEFAULT '' NOT NULL,
+	`text` text,
+	`html` text,
+	`raw` text,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer DEFAULT (unixepoch()) NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE `objects` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`key` text NOT NULL,
