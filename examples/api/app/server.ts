19ff7245927ac46bc1724d2c06b56955ab8f7d3d
Arik Chakma <arikchangma@gmail.com>
2026-04-04 19:00:20 +0600

    feat: add cors middleware for kumoh.app

--- examples/api/app/server.ts
+++ examples/api/app/server.ts
@@ -1,6 +1,16 @@
+import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 import { defineApp } from 'kumoh/app';
 
 export default defineApp((app) => {
   app.use(logger());
+  app.use(
+    cors({
+      origin: 'https://kumoh.app',
+      allowHeaders: ['Content-Type', 'Authorization'],
+      allowMethods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+      credentials: true,
+      maxAge: 3600,
+    })
+  );
 });
