7fac64581a2041c590280916f9d7a2a3acaeac20
Arik Chakma <arikchangma@gmail.com>
2026-04-04 20:07:20 +0600

    fix: add sandbox.kumoh.dev to cors origins

--- examples/api/app/server.ts
+++ examples/api/app/server.ts
@@ -6,7 +6,7 @@ export default defineApp((app) => {
   app.use(logger());
   app.use(
     cors({
-      origin: 'https://kumoh.dev',
+      origin: ['https://kumoh.dev', 'https://sandbox.kumoh.dev'],
       allowHeaders: ['Content-Type', 'Authorization'],
       allowMethods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
       credentials: true,
